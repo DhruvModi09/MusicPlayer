a0b5bf0fc6d3657e8fb833235a4bf01ffb2cbd25
SujalShah3234 <shahc9437@gmail.com>
2021-11-29 14:15:41 +0530

    Crash fix: Null check added!

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,18 +15,14 @@ import android.widget.TextView;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
 
     ListView listView;
     String[] items;
@@ -43,70 +38,59 @@ public class MainActivity extends AppCompatActivity {
 
     public void runtimePermission()
     {
-        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO)
-                .withListener(new MultiplePermissionsListener() {
-                    @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
-                        displaySong();
-                    }
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                displaySong();
+            }
 
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
-                        permissionToken.continuePermissionRequest();
-                    }
-                }).check();
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
     }
 
-    public ArrayList<File> findSong(File file)
-    {
+    public ArrayList<File> findSong(File file) {
         ArrayList<File> arrayList = new ArrayList<>();
         File[] files = file.listFiles();
 
-        for (File singleFile : files )
-        {
-            if(singleFile.isDirectory() && !singleFile.isHidden())
-            {
-                arrayList.addAll(findSong(singleFile));
-            }
-            else
-            {
-                if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav"))
-                {
-                    arrayList.add(singleFile);
+        if (files != null) {
+            for (File singleFile : files) {
+                if(singleFile.isDirectory() && !singleFile.isHidden()) {
+                    arrayList.addAll(findSong(singleFile));
+                }
+                else {
+                    if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                        arrayList.add(singleFile);
+                    }
                 }
             }
         }
         return arrayList;
     }
 
-    public void displaySong()
-    {
+    public void displaySong() {
         final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
         items = new String[mySongs.size()];
-        for (int i = 0; i < mySongs.size(); i++)
-        {
+        for (int i = 0; i < mySongs.size(); i++) {
             items[i] = mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav", "");
         }
 
         customAdapter customAdapter = new customAdapter();
         listView.setAdapter(customAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String songName = (String) listView.getItemAtPosition(position);
-                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
-                .putExtra("songs", mySongs)
-                        .putExtra("songname", songName)
-                        .putExtra("pos", position)
-                );
-            }
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String songName = (String) listView.getItemAtPosition(position);
+            startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+            .putExtra("songs", mySongs)
+                    .putExtra("songname", songName)
+                    .putExtra("pos", position)
+            );
         });
     }
 
-    class customAdapter extends BaseAdapter
-    {
-
+    class customAdapter extends BaseAdapter {
         @Override
         public int getCount() {
             return items.length;
@@ -132,5 +116,4 @@ public class MainActivity extends AppCompatActivity {
             return view;
         }
     }
-
 }
